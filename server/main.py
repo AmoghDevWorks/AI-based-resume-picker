14953f96818d73a9e6c56e6bb1ac5b9278f946b6
AmoghDevWorks <fakemail2k05@gmail.com>
2026-06-18 14:37:26 +0530

    cors handling

--- server/main.py
+++ server/main.py
@@ -1,24 +1,23 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from routes.ranking import (
-    router as ranking_router
-)
+from routes.ranking import router as ranking_router
 
-app = FastAPI(
-    title="Redrob Candidate Ranking System"
+app = FastAPI(title="Redrob Candidate Ranking System")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("FRONTEND_URL", "*")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
 @app.get("/")
 async def root():
-
-    return {
-        "message": "API is running"
-    }
+    return {"message": "API is running"}
 
 
-app.include_router(
-    ranking_router,
-    prefix="/ranking",
-    tags=["Ranking"]
-)
+app.include_router(ranking_router, prefix="/ranking", tags=["Ranking"])
